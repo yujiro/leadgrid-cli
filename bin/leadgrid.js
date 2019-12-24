b5bca55544d37a9e9bce6c91901abcfd6059e322
yujiro-arai <arai_yujiro@giginc.co.jp>
2019-12-24 15:18:10 +0900

    auto restart

--- bin/leadgrid.js
+++ bin/leadgrid.js
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
-require('../dist/serve.js');
+var nodemon = require('nodemon');
+var figlet = require('figlet')
+var chalk = require('chalk')
+
+console.log(
+  chalk.yellow(
+    figlet.textSync('LeadGrid', { horizontalLayout: 'full' })
+  )
+);
+
+nodemon({
+  script: __dirname + '/../dist/serve.js',
+  ext: 'js json hbs yml js css'
+});
+
+nodemon.on('start', function () {
+  console.log('LeadGrid has started');
+}).on('quit', function () {
+  console.log('LeadGrid has quit');
+  process.exit();
+}).on('restart', function (files) {
+  console.log('LeadGrid restarted due to: ', files);
+});
