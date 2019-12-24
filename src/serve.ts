aab2bd8d377a68759c818a21b337043874a55979
yujiro-arai <arai_yujiro@giginc.co.jp>
2019-12-24 15:17:59 +0900

    show error reason

--- src/serve.ts
+++ src/serve.ts
@@ -2,7 +2,6 @@ import * as express from 'express'
 import * as yaml from 'js-yaml'
 import * as fs from 'fs'
 import Handlebars from 'handlebars'
-import * as figlet from 'figlet'
 import * as chalk from 'chalk'
 
 const rootDir = process.cwd()
@@ -13,12 +12,6 @@ const componentsDir = `${rootDir}/components`
 const routeFilePath = `${rootDir}/routes.yml`
 const publicDir = `${rootDir}/assets`
 const port = 5000
-
-console.log(
-  chalk.yellow(
-    figlet.textSync('LeadGrid', { horizontalLayout: 'full' })
-  )
-);
 
 const app = express()
 
@@ -31,19 +24,32 @@ fs.readdir(componentsDir, (error, files = []) => {
   
   pages.forEach((page: any) => {
     const doc = yaml.safeLoad(fs.readFileSync(`${pagesDir}/${page.page}.yml`, 'utf8'));
-    const layout = fs.readFileSync(`${layoutDir}/${doc.layout}.hbs`, 'utf8')
+    const layoutPath = `${layoutDir}/${doc.layout}.hbs`
+    const layout = fs.readFileSync(layoutPath, 'utf8')
     const template = Handlebars.compile(layout)
+
     const sections = doc.sections.map((section: string) => {
-      const sectionTemplate = Handlebars.compile(fs.readFileSync(`${sectionsDir}/${section}.hbs`, 'utf8'))
-      return sectionTemplate({})
+      const filePath = `${sectionsDir}/${section}.hbs`
+      const sectionTemplate = Handlebars.compile(fs.readFileSync(filePath, 'utf8'))
+
+      try {
+        return sectionTemplate({})  
+      } catch (e) {
+        console.error(`Parse error! check this section file ---> ${filePath}`)
+        throw e
+      }
     }).join('')
   
-    const html = template({sections});
+    try {
+      const html = template({sections});
   
-    app.get(page.path, (_, res) => res.send(html))
+      app.get(page.path, (_, res) => res.send(html))
+    } catch (e) {
+      console.error(chalk.red(`Parse error! check this layout file ---> ${layoutPath}`))
+      throw e
+    }
   });
   
-
   app.use('/assets', express.static(publicDir));
   app.listen(port, () => console.log(`LeadGrid simulator listening on port ${port}!`))
 })
