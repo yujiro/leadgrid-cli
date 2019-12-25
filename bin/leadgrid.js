b975aebdc22023f9245d019a19a6e12df90435fb
yujiro-arai <arai_yujiro@giginc.co.jp>
2019-12-25 10:46:13 +0900

    change leadgrid command signature
    leadgrid -> leadgrid watch

--- bin/leadgrid.js
+++ bin/leadgrid.js
@@ -4,22 +4,33 @@ var nodemon = require('nodemon');
 var figlet = require('figlet')
 var chalk = require('chalk')
 
-console.log(
-  chalk.yellow(
-    figlet.textSync('LeadGrid', { horizontalLayout: 'full' })
-  )
-);
+require('yargs')
+  .command('watch', 'start the server and watch update files', (yargs) => {
+    watch()
+  })
+  .command('deploy', 'deploy files', (yargs) => {
+    console.log({yargs})
+  })
+  .argv
 
-nodemon({
-  script: __dirname + '/../dist/serve.js',
-  ext: 'js json ts hbs yml js css'
-});
+function watch() {
+  console.log(
+    chalk.yellow(
+      figlet.textSync('LeadGrid', { horizontalLayout: 'full' })
+    )
+  );
 
-nodemon.on('start', function () {
-  console.log('LeadGrid has started');
-}).on('quit', function () {
-  console.log('LeadGrid has quit');
-  process.exit();
-}).on('restart', function (files) {
-  console.log('LeadGrid restarted due to: ', files);
-});
+  nodemon({
+    script: __dirname + '/../dist/serve.js',
+    ext: 'js json ts hbs yml js css'
+  });
+  
+  nodemon.on('start', function () {
+    console.log('LeadGrid has started');
+  }).on('quit', function () {
+    console.log('LeadGrid has quit');
+    process.exit();
+  }).on('restart', function (files) {
+    console.log('LeadGrid restarted due to: ', files);
+  });
+}
